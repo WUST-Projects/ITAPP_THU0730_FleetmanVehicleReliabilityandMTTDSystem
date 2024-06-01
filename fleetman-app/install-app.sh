f5c0b9b2bf797f42bfa942f04309d6cb45b4742e
WUST-Project <283500@student.pwr.edu.pl>
2024-06-01 17:46:10 +0200

    fix: version on fleetman-app deployment

--- fleetman-app/install-app.sh
+++ fleetman-app/install-app.sh
@@ -3,10 +3,10 @@
 . ../config.sh
 
 # Install backend micro services
-# kubectl config use-context ${CLUSTER2_CTX}
-# kubectl label ns default istio-injection=enabled --overwrite
-kubectl -n default apply -f pvc.yaml
-kubectl -n default apply -f mongo-stack.yaml
+kubectl config use-context ${CLUSTER2_CTX}
+kubectl label ns default istio-injection=enabled --overwrite
+# kubectl -n default apply -f pvc.yaml
+# kubectl -n default apply -f mongo-stack.yaml
 kubectl -n default apply -f workloads.yaml
 kubectl -n default apply -f services.yaml
 
