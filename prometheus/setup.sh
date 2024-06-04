1e7321f8b2e1a970e32af7579bd8dd1eba53e00c
WUST-Project <283500@student.pwr.edu.pl>
2024-06-04 08:20:11 +0200

    chore: add prometheus config

---
+++ prometheus/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Setup instruction is found here: https://devopscube.com/setup-prometheus-monitoring-on-kubernetes/
+. ../config.sh
+
+kubectl config use-context ${CLUSTER2_CTX}
+kubectl create namespace monitoring
+kubectl label ns monitoring istio-injection=enabled --overwrite
+kubectl apply -f clusterRole.yaml
+kubectl apply -f prometheus-config.yaml
+kubectl apply -f prometheus-deployment.yaml
+# Install kube-state-metrics
+kubectl -n kube-system apply -f kube-state-metrics/
